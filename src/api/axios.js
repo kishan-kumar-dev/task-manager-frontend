530299c4e4573a58d38ab63e84e68a6bdf964bb1
Kishan Kumar <kishankumar.modi.3@gmail.com>
2025-11-28 09:34:14 +0530

    Added env configs and updated axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-// Attach token automatically
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
